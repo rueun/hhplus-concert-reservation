3dd0578b89fecd8e7fab15c2a263ca3e6396a3d6
rueun <dkfah1229@naver.com>
2024-10-09 19:04:16 +0900

    init2

---
+++ src/test/java/com/hhplus/concertreservation/ConcertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.concertreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
