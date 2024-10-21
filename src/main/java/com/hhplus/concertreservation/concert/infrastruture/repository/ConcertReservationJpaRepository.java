9ec4532cb946ed2ff7daebdec15f819fd8b87bc6
rueun <dkfah1229@naver.com>
2024-10-21 21:51:55 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/hhplus/concertreservation/concert/infrastruture/repository/ConcertReservationJpaRepository.java
+++ src/main/java/com/hhplus/concertreservation/concert/infrastruture/repository/ConcertReservationJpaRepository.java
@@ -1,7 +1,6 @@
 package com.hhplus.concertreservation.concert.infrastruture.repository;
 
 import com.hhplus.concertreservation.concert.infrastruture.entity.ConcertReservationEntity;
-import com.hhplus.concertreservation.concert.infrastruture.entity.ConcertSeatEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConcertReservationJpaRepository extends JpaRepository<ConcertReservationEntity, Long> {
