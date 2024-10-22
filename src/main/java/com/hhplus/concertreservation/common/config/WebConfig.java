bac7559e49f48ef680af88c8247dd26375460c1b
rueun <dkfah1229@naver.com>
2024-10-23 01:33:07 +0900

    feat: 대기열 토큰 검증 인터셉터 구현

---
+++ src/main/java/com/hhplus/concertreservation/common/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.hhplus.concertreservation.common.config;
+
+import com.hhplus.concertreservation.support.api.intercepter.QueueTokenInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final QueueTokenInterceptor queueTokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(queueTokenInterceptor)
+                .addPathPatterns("/api/v1/payments/**")
+                .addPathPatterns("/api/v1/concerts/**");
+    }
+}
