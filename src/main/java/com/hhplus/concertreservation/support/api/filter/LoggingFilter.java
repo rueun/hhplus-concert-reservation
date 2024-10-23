683f9bb0a35e595b009bf14d67fb43c7c7e0df9f
rueun <dkfah1229@naver.com>
2024-10-23 23:43:07 +0900

    feat: 로깅 필터 - 제외할 url 설정

--- src/main/java/com/hhplus/concertreservation/support/api/filter/LoggingFilter.java
+++ src/main/java/com/hhplus/concertreservation/support/api/filter/LoggingFilter.java
@@ -10,6 +10,7 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 public class LoggingFilter extends OncePerRequestFilter {
@@ -18,18 +19,27 @@ public class LoggingFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         ContentCachingRequestWrapper httpServletRequest = new ContentCachingRequestWrapper(request);
         ContentCachingResponseWrapper httpServletResponse = new ContentCachingResponseWrapper(response);
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
 
         // request 로깅
         String uri = httpServletRequest.getRequestURI();
         String reqContent = new String(httpServletRequest.getContentAsByteArray());
         log.info("request uri : {}, request body: {}", uri, reqContent);
 
+        // 다음 필터로 요청을 전달
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
         // response 로깅
         int httpStatus = httpServletResponse.getStatus();
         String resContent = new String(httpServletResponse.getContentAsByteArray());
         httpServletResponse.copyBodyToResponse();
         log.info("response status: {}, response body {}", httpStatus, resContent);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String[] excludePath = {"/swagger-ui", "/v3/api-docs"};
+        String path = request.getRequestURI();
+        return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
 }
 
