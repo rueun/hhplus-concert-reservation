6a978667e95f8ef65178ed5b60fcd85bc0974397
rueun <dkfah1229@naver.com>
2024-11-15 03:44:53 +0900

    chore: 비동기 관련 설정 추가

---
+++ src/main/java/com/hhplus/concertreservation/common/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.hhplus.concertreservation.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean("customAsyncExecutor")
+    public Executor customAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setThreadNamePrefix("thread-");
+        executor.initialize();
+        return executor;
+    }
+}
