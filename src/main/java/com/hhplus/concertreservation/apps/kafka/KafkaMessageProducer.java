01a7405230ee7d24bac5e5433d9ff4d3d673b5b2
rueun <dkfah1229@naver.com>
2024-11-20 11:19:22 +0900

    feat: 스프링 카프카 연동

---
+++ src/main/java/com/hhplus/concertreservation/apps/kafka/KafkaMessageProducer.java
@@ -0,0 +1,39 @@
+package com.hhplus.concertreservation.apps.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaMessageProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public <T> void send(String topic, T message) {
+        try {
+            String json = objectMapper.writeValueAsString(message);
+            kafkaTemplate.send(topic, json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to serialize message", e);
+        }
+    }
+
+    public <T> void send(String topic, String key, T message) {
+        try {
+            String json = objectMapper.writeValueAsString(message);
+            kafkaTemplate.send(topic, key, json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to serialize message", e);
+        }
+    }
+
+    @PostConstruct
+    public void init() {
+        send("test-topic", "Hello, Kafka!");
+        send("test-topic", "key", "Hello, Kafka! with key");
+    }
+}
