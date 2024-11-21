96b8a05f6e0d7320467036e03211b93df294b8d0
rueun <dkfah1229@naver.com>
2024-11-22 04:55:36 +0900

    fix: 아웃박스 스케줄러 카프카 프로듀스 하도록 변경

--- src/main/java/com/hhplus/concertreservation/apps/concert/scheduler/OutBoxScheduler.java
+++ src/main/java/com/hhplus/concertreservation/apps/concert/scheduler/OutBoxScheduler.java
@@ -1,7 +1,9 @@
 package com.hhplus.concertreservation.apps.concert.scheduler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hhplus.concertreservation.apps.concert.domain.model.entity.Outbox;
 import com.hhplus.concertreservation.apps.concert.domain.service.OutboxService;
+import com.hhplus.concertreservation.apps.kafka.KafkaMessageProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,14 +17,17 @@ import java.util.List;
 public class OutBoxScheduler {
 
     private final OutboxService outboxService;
+    private final KafkaMessageProducer kafkaMessageProducer;
+    private final ObjectMapper objectMapper;
 
-    @Scheduled(fixedDelayString = "10000")
+    @Scheduled(fixedDelayString = "60000")
     public void rePublishMessageInitState() {
         log.info("Outbox 대기 중인 이벤트 발송 스케줄러 실행");
         final List<Outbox> outboxes = outboxService.findAllAfterThreshold(5);
         outboxes.forEach(outbox -> {
             try {
-                outboxService.publish(outbox.getEventKey());
+                final Object event = objectMapper.readValue(outbox.getPayload(), Object.class);
+                kafkaMessageProducer.send("concert.concert-reserved", outbox.getEventKey(), event);
             } catch (Exception e) {
                 log.error("Outbox 이벤트 발송 중 오류 발생 (ID: {}): {}", outbox.getId(), e.getMessage());
             }
